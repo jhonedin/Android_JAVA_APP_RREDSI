82631a28c7e09017857341855be7877027c8100a
jhonedin <jhonedin1888@hotmail.com>
2019-09-25 12:06:06 -0500

    Se añadio la listview a las consultas

--- app/src/main/java/com/example/apprredsi/RespuestaConsultasActivity.java
+++ app/src/main/java/com/example/apprredsi/RespuestaConsultasActivity.java
@@ -1,14 +1,14 @@
 package com.example.apprredsi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.os.StrictMode;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,33 +21,35 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class RespuestaConsultasActivity extends AppCompatActivity {
 
     Button btnVolverToMain;
-
+    /*
     TextView textViewPrueba;
     TextView textViewPrueba2;
     TextView textViewPrueba3;
     TextView textViewPrueba4;
     TextView textViewPrueba5;
-
     TextView sal;
+    */
+
+    ListView listaProyectos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_respuesta_consultas);
-
+        /*
         textViewPrueba =  findViewById(R.id.textViewPrueba);
         textViewPrueba2 = findViewById(R.id.textViewPrueba2);
         textViewPrueba3 = findViewById(R.id.textViewPrueba3);
         textViewPrueba4 = findViewById(R.id.textViewPrueba4);
         textViewPrueba5 = findViewById(R.id.textViewPrueba5);
-
         sal  = (TextView) findViewById(R.id.textViewSalidaApi);
-
-
+        */
+        listaProyectos = findViewById(R.id.listViewProyectos);
 
         Bundle delivery = getIntent().getExtras();
         String evaluador = delivery.getString("evaluador");
@@ -55,12 +57,12 @@ public class RespuestaConsultasActivity extends AppCompatActivity {
         String autores = delivery.getString("autores");
         String areasconocimiento = delivery.getString("areasconocimiento");
         String nombrepropuesta = delivery.getString("nombrepropuesta");
-
+        /*
         textViewPrueba.setText(evaluador);
         textViewPrueba2.setText(institucion);
         textViewPrueba3.setText(autores);
         textViewPrueba4.setText(areasconocimiento);
-        textViewPrueba5.setText(nombrepropuesta);
+        textViewPrueba5.setText(nombrepropuesta); */
 
 
 
@@ -72,31 +74,28 @@ public class RespuestaConsultasActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
-        getData();
+        String proyectos[] = getData(evaluador,institucion,autores,areasconocimiento,nombrepropuesta);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, proyectos);
+        listaProyectos.setAdapter(arrayAdapter);
+
     }
 
-    public void getData(){
-        String sql = "http://www.mocky.io/v2/5d6addff3100006000d2077c";
-
+    public String[] getData(String evaluador,String institucion,String autores,String areasconocimiento,String nombrepropuesta){
+       // String sql = "http://www.mocky.io/v2/5d6addff3100006000d2077c"; // de prueba del profe
+        String sql = "http://www.mocky.io/v2/5d8a38273000008cc2b9a81d";
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
         HttpURLConnection conn;
-
+        ArrayList<String> arrayProyectos = new ArrayList<>();
         try {
             URL url = new URL(sql);
             conn = (HttpURLConnection) url.openConnection();
-
             conn.setRequestMethod("GET");
-
             conn.connect();
-
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
             String inputLine;
-
             StringBuffer response = new StringBuffer();
-
             String json = "";
 
             while((inputLine = in.readLine()) != null){
@@ -105,19 +104,24 @@ public class RespuestaConsultasActivity extends AppCompatActivity {
 
             json = response.toString();
 
-            //JSONArray jsonArr = new JSONArray(json);
+            JSONObject object = new JSONObject(json);
+            JSONArray json_array = object.optJSONArray("agenda");
+
             //String mensaje = "";
-            /*
-            for(int i = 0;i<jsonArr.length();i++){
-                JSONObject jsonObject = jsonArr.getJSONObject(i);
 
-                Log.d("SALIDA",jsonObject.optString("agenda"));
-                mensaje += ("DESCRIPCION " + i + " " + jsonObject.optString("espacio") + "\n");
+            for(int i=0; i < json_array.length(); i++){
+                JSONObject unObject = json_array.getJSONObject(i);
+                //JSONArray json_array_Trabajo =  unObject.optJSONArray("trabajo");
+                //JSONObject ObjectTrabajo = json_array_Trabajo.getJSONObject(i);
+                //Log.d("mensaje",unObject.get("id_agenda") .toString());
+                //arrayProyectos.add(unObject.get("id_agenda").toString());
+                //mensaje += json_array.getJSONObject(i).toString();
+                JSONObject objectTitulo = (JSONObject) unObject.get("trabajo");
+                arrayProyectos.add(objectTitulo.get("titulo").toString());
             }
-
-             */
             //sal.setText(mensaje);
-            sal.setText(json);
+
+
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -125,9 +129,12 @@ public class RespuestaConsultasActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } 
-
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return arrayProyectos.toArray(new String[0]);
     }
+
 
 
 }
